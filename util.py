227020a1749a10bac6616368b06d5962587488ae
pavlovicmilena <milenapavlovic775@gmail.com>
2022-06-06 20:02:05 +0200

    update exercises

---
+++ util.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def count_outcomes(outcomes):
+    outcome_summary = {}
+    for outcome in outcomes:
+        if outcome in outcome_summary:
+            outcome_summary[outcome] += 1
+        else:
+            outcome_summary[outcome] = 1
+    return outcome_summary
+
+
+def plot_outcomes(outcomes, normalize: bool = False):
+    outcomes_summary = count_outcomes(outcomes)
+    fig, ax = plt.subplots(clear=True)
+    x_values = list(outcomes_summary.keys())
+
+    if normalize:
+        toss_count = sum(list(outcomes_summary.values()))
+        y_values = np.array(list(outcomes_summary.values())) / toss_count
+    else:
+        y_values = list(outcomes_summary.values())
+
+    ax.bar(x_values, y_values)
+    ax.set_xticks(x_values)
+
+
+def plot_event_probabilities(event_probabilities: dict):
+    fig, ax = plt.subplots(clear=True)
+    x_values = list(event_probabilities.keys())
+    y_values = list(event_probabilities.values())
+    ax.bar(x_values, y_values)
+    ax.set_xticks(x_values)
